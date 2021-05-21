ebb1e7665d76f78c674a7887ecab374c8d2ffe6e
Shaun <shauncuier@gmail.com>
2021-05-22 05:39:15 +0600

    Home Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,33 @@
 @extends('layouts.app')
 @section('content')
-<div>
-    <div>
-        <div>
-            
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Do you want to become a developer?
+                </h1>
+                <a href="/blog" class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    Read more
+                </a>
+            </div>
         </div>
     </div>
-</div>
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+        </div>
+        <div class="m-auto sm:m-auto text-left w-4/5 block">
+            <h2 class="text-4xl font-extrabold text-gray-600">
+                Struggling to be a better web developer?
+            </h2>
+            <p class="py-8 text-gray-500 text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus molestiae ipsum totam! Minima, adipisci. Ab, impedit! Esse aliquam nisi corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, possimus!
+            </p>
+            <p class="font-extrabold text-gray-600 text-xl pb-9">
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod, suscipit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum minus eius cumque officiis consequuntur aperiam quas provident ratione debitis nesciunt!
+            </p>
+            <a href="/blog"
+            class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">Find Out More</a>
+        </div>
+    </div>
 @endsection
-
