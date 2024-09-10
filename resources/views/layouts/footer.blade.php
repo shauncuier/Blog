124dae7aff5da68eabd8bb7fd617ffe95833b4e4
Jashedul Islam Shaun <shauncuier@gmail.com>
2024-09-11 00:24:39 +0600

    modified:   resources/views/layouts/footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -63,6 +63,6 @@
         </div>
     </div>
     <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6 text-center">
-        Copyright &copy; {{date("Y")}} | <a href="3s-soft.com">3s-Soft</a> | All Rights reserved
+        Copyright &copy; {{date("Y")}} | <a href="https://3s-soft.com">3s-Soft</a> | All Rights reserved
     </p>
 </footer>
